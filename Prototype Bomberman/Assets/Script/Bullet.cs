5382873fc4453cbbaeb68bcb45f76eb3b736f876
DavidCh98 <47038868+DavidCh98@users.noreply.github.com>
2019-09-18 15:25:47 +0200

    VersionDavid

---
+++ Prototype Bomberman/Assets/Script/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Transform character;
+    Animator animator;
+    public float speed;
+    public float distance;
+    public LayerMask whatIsSolid;
+    public GameObject bullet;
+    public float targetTime = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GameObject.Find("CharacterA").GetComponent<Transform>();
+        animator = GameObject.Find("CharacterA").GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.forward, distance, whatIsSolid);
+       if (hitInfo.collider != null)
+       {
+           Destroy(gameObject);
+           
+       }
+       if (character.transform.localRotation == Quaternion.Euler(0, 0, 0)){
+            transform.Translate(transform.right * speed * Time.deltaTime);
+       }
+       else if(animator.GetBool("CharacterAnimation") == false ||animator.GetBool("CharacterAnimation") == true){
+            transform.Translate(-transform.right * speed * Time.deltaTime);
+       }
+        targetTime -= Time.deltaTime;
+ 
+            if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+        
+    }
+        
+        void timerEnded()
+        {
+            Destroy(gameObject);
+        }
+}
+
---
+++ Prototype Bomberman/Assets/Script/MovementCharacterA.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovementCharacterA : MonoBehaviour
+{
+    Transform character;
+    public float speed;
+    Animator animator;
+    bool firstJump;
+    bool secondJump;
+    GameObject gunObject;
+    Renderer gun;
+    public GameObject bullet;
+    public Transform shotPoint;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GameObject.Find("CharacterA").GetComponent<Transform>(); 
+        animator = GetComponent<Animator>(); 
+        gunObject = character.transform.GetChild(0).gameObject; 
+        gun = gunObject.GetComponent<Renderer>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = Vector2.zero;
+        if (firstJump == true && secondJump == false && Input.GetKeyDown(KeyCode.W) == true){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 5.0f); 
+            Debug.Log("firstJump"); 
+            firstJump = false;
+            secondJump = true;
+        }
+        else if (firstJump == false && secondJump == true && Input.GetKeyDown(KeyCode.W) == true){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 6.5f); 
+            Debug.Log("secondJump");
+            secondJump = false;
+        } 
+         
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            movement = Vector2.right * speed;
+            animator.SetBool("CharacterAnimation",true);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            movement = Vector2.right * speed;
+            animator.SetBool("CharacterAnimation",true);
+        }
+        else
+        {
+            animator.SetBool("CharacterAnimation",false);
+        }
+        
+        character.Translate(movement);
+        if(timeBtwShots <= 0){
+            if(Input.GetKeyDown(KeyCode.F) && gun.enabled == true)
+            {
+                Instantiate(bullet,shotPoint.position,transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }else{
+            timeBtwShots -= Time.deltaTime;
+        }
+         if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+        }     
+    }
+    void OnCollisionEnter2D(Collision2D col) {
+        if(col.gameObject.name == "TilemapGround")
+        {
+            firstJump = true;
+            secondJump = false;
+            Debug.Log("I hit the ground");
+        }
+        if(col.gameObject.name == "box" || col.gameObject.name == "gun")
+        {
+           Destroy(col.gameObject);
+           gun.enabled = true;
+        }
+    } 
+      
+}
+
+ 
+    
