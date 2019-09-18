a7e63940b80e416dd9f10a05b6105749d198eec9
Christian <c.schawe@web.de>
2019-09-18 16:00:43 +0200

    actual merge of the projects

---
+++ Prototype Bomberman/Assets/Script/LifeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeController : MonoBehaviour
+{
+    public int lives;
+    
+    [SerializeField] 
+    private GameObject[] hearts;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = 3;
+        if (this.name == "CharacterA")
+        {
+            hearts = GameObject.FindGameObjectsWithTag("HeartP1");
+        } else if (this.name == "CharacterB")
+        {
+            hearts = GameObject.FindGameObjectsWithTag("HeartP2");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(lives)
+        {
+            case 2:
+                hearts[hearts.Length-1].gameObject.SetActive(false);
+                break;
+            case 1:
+                hearts[hearts.Length-2].gameObject.SetActive(false);
+                break;
+            case 0:
+                hearts[hearts.Length-3].gameObject.SetActive(false);
+                break;
+        }
+    }
+}
---
+++ Prototype Bomberman/Assets/Script/ScoreController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+
+    [SerializeField]
+    public int score;
+    [SerializeField]
+    private Text scoreField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreField.text = "Score: " + score;   
+    }
+}
