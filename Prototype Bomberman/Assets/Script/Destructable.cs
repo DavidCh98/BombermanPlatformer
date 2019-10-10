261f16833de304d7a2ab5263632783fe36398cea
Christian <c.schawe@web.de>
2019-10-10 11:44:40 +0200

    Add random spawn of items

--- Prototype Bomberman/Assets/Script/Destructable.cs
+++ Prototype Bomberman/Assets/Script/Destructable.cs
@@ -6,6 +6,9 @@ using UnityEngine.Tilemaps;
 public class Destructable : MonoBehaviour
 {
    public GameObject bullet;
+   public GameObject[] items;
+   public Vector2 objectPos;
+   public Vector3Int celPos;
    void OnCollisionEnter2D(Collision2D other)
     {
         // the tilemap component
@@ -15,13 +18,33 @@ public class Destructable : MonoBehaviour
         // get one of the contact's position (I suppose any position would do)
         Vector3 cp = other.GetContact(0).point;
         // convert from world position to grid position
-        Vector3Int celPos = grid.WorldToCell(new Vector3(cp.x, cp.y, 0));
+        celPos = grid.WorldToCell(new Vector3(cp.x, cp.y, 0));
         // remove the tile from the tilemap
         if (other.gameObject.tag == "bullet"){
             tm.SetTile(celPos, null);
-            
+            SpawnItem();
         }
         Debug.Log(other.gameObject.tag);
            
+    }
+
+    private void SpawnItem ()
+    {
+        Bullet bullet = GetComponent<Bullet>();
+        int randomItem = Random.Range(0,4);
+        int spawnChance = Random.Range(0, 101); 
+        objectPos = new Vector2(celPos.x + 0.5f, celPos.y+0.5f);
+
+        switch(spawnChance)
+        {
+            case int n when(n <=30):
+                Instantiate(items[randomItem], objectPos, Quaternion.identity); 
+                break;
+            case int n when(n > 50 && n <= 100):
+                break;
+        }
+        Debug.Log(randomItem + " " + spawnChance);
+
+
     }
 }
