b44b2569119fce3e8d26c5eaddcbc896fc630f40
Pat Cleaver <pat.cleaver.bass@gmail.com>
2019-10-16 15:59:13 +0200

    Added Ambiance player object and script (fade in at start plus fade out on destroy)

---
+++ Prototype Bomberman/Assets/Script/AmbiancePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbiancePlayer : MonoBehaviour
+{
+
+    FMOD.Studio.EventInstance Ambiance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ambiance = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/JungleAmbiance");
+        Ambiance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        Ambiance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
