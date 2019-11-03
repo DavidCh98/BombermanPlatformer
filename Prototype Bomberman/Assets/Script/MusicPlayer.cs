7b60110b2f583d3a0a177510fe63b1ae14a2e8cf
Pat Cleaver <pat.cleaver.bass@gmail.com>
2019-11-03 15:03:40 +0100

    Added music player object and music player script, plus AHDSR settings for music

---
+++ Prototype Bomberman/Assets/Script/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    FMOD.Studio.EventInstance GameplayMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameplayMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/GameplayMusic");
+        GameplayMusic.start();
+        GameplayMusic.release();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        GameplayMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
