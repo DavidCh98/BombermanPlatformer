10aec22dddf1323d1d8671fde6caead788197fe7
DavidCh98 <47038868+DavidCh98@users.noreply.github.com>
2019-11-04 13:04:55 +0100

    Explosion particle effects added

--- Prototype Bomberman/Assets/Script/Bullet.cs
+++ Prototype Bomberman/Assets/Script/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject bullet;
+    public GameObject destroyEffect;
     [SerializeField]
     public float flyTime;
     // Start is called before the first frame update
@@ -24,7 +25,7 @@ public class Bullet : MonoBehaviour
         }
     }
     private void OnCollisionEnter2D(Collision2D other) {
-        
+        Instantiate(destroyEffect,transform.position, Quaternion.identity);
         Destroy(gameObject);
         if (other.gameObject.tag == "Player")
         {
